a42ed97d5a734c5f336178d60038923b4863d92c
Mark Vallis <markvallis1987@Gmail.com>
2018-02-18 19:49:43 +0000

    quadratic calculator returning results although x1 and x2 seem to be the same

--- venv/QuadraticCalculator.py
+++ venv/QuadraticCalculator.py
@@ -1,3 +1,15 @@
-def quadCalc(n1, n2):
-    xAndYList = [2,3]
-    return xAndYList
+import cmath
+
+
+def quadCalc(a, b, c):
+    middle = sqrtCalc(a, b, c)
+    x1 = (-b + (cmath.sqrt(middle))) / (2 * a)
+    x2 = (-b - (cmath.sqrt(middle))) / (2 * a)
+    x_list = [x1, x2]
+    return x_list
+
+
+def sqrtCalc(a, b, c):
+    b_squared = b ** b
+    four_a_c = 4 * a * c
+    return b_squared - four_a_c
