d417cfeeaf92f768f7e0efde8302427fb47c9017
Mark Vallis <markvallis1987@Gmail.com>
2018-02-18 17:13:07 +0000

    Started setting up quadratic calculator class

--- venv/Calculator.py
+++ venv/Calculator.py
@@ -1,11 +1,14 @@
-from MathsTools import *
 from sys import version_info
+from MathsTools import *
+from QuadraticCalculator import quadCalc
 
 py3 = version_info[0] > 2
 
 if py3:
-    num1 = int(input("num1: "))
-    num2 = int(input("num2: "))
+    num1 = 2
+    num2 = 6
+    # num1 = int(input("num1: "))
+    # num2 = int(input("num2: "))
 else:
     num1 = int(raw_input("num1: "))
     num2 = int(raw_input("num2: "))
@@ -24,3 +27,6 @@ listOfNumbers = [1,2,3,4,5]
 
 print("Multiplying two numbers (using value from list)", multiply(listOfNumbers[2], num2))
 
+print("Quadratic calculator code being called here")
+for item in quadCalc(num1, num2):
+    print(item)
---
+++ venv/QuadraticCalculator.py
@@ -0,0 +1,3 @@
+def quadCalc(n1, n2):
+    xAndYList = [2,3]
+    return xAndYList
