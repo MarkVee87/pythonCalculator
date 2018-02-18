aa578935ebf47d294506d39c8d480ac1334d73ca
Mark Vallis <markvallis1987@Gmail.com>
2018-02-18 20:56:10 +0000

    graphs now being produced for quadratic equation results

--- venv/Calculator.py
+++ venv/Calculator.py
@@ -1,5 +1,6 @@
 from MathsTools import *
 from QuadraticCalculator import quadratic_calculator
+from CreateGraphs import *
 
 num1 = 3
 num2 = 7
@@ -12,4 +13,10 @@ print("Raising a number to a power", raiseToPowerOf(num1, num2))
 
 print("\nQuadratic calculator:")
 quadraticCoordinates = quadratic_calculator()
-print('x1 = {0} and x2 = {1}'.format(quadraticCoordinates[0], quadraticCoordinates[1]))
+
+x1 = quadraticCoordinates[0]
+x2 = quadraticCoordinates[1]
+
+print('x1 = {0} and x2 = {1}'.format(x1, x2))
+
+quadratic_graph(x1, x2)
