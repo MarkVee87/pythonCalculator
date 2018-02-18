08a4d39e0a8e44e136a8aa4a22621401755a906e
Mark Vallis <markvallis1987@Gmail.com>
2018-02-18 23:37:47 +0000

    vertexes now shown in correct order - just need curved lines and to make parabola visible to top of graph

--- venv/CreateGraphs.py
+++ venv/CreateGraphs.py
@@ -3,7 +3,7 @@ import numpy
 
 
 def quadratic_graph(x1, x2, vx, vy):
-    plt.plot([numpy.real(x1), numpy.real(x2), vx], [0, 0, vy], 'ro')
+    plt.plot([numpy.real(x1), vx, numpy.real(x2)], [0, vy, 0], lw=2)
     plt.axis([-10, 10, -10, 10])
     plt.xlabel('x')
     plt.ylabel('y')
